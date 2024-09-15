1d203f4c3690f173efb3e022731296e4bed00b68
HernanR <ruizhernan@tecnica7.edu.ar>
2024-09-15 20:22:41 -0300

    Se agregan componentes

---
+++ src/components/NavBar.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="nav-bar">
+      <CustomButton
+        text="All"
+        icon="src/assets/list.png"
+        :backgroundColor="'#BFBFBF'"
+        @click="showAllPokemons"
+      />
+      <CustomButton
+        text="Favorites"
+        icon="src/assets/seeFavs.png"
+        @click="showFavorites"
+      />
+    </div>
+  </template>
+  
+  <script>
+  import CustomButton from "@/components/CustomButton.vue";
+  
+  export default {
+    components: {
+      CustomButton
+    },
+    methods: {
+      showFavorites() {
+        this.$emit('show-favorites');
+      },
+      showAllPokemons() {
+        this.$emit('show-all');
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .nav-bar {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    height: 80px;
+    background-color: #ffffff;
+    box-shadow: 0px -2px 5px rgba(0, 0, 0, 0.1);
+    margin-top: auto; 
+  }
+  </style>
+  
--- src/views/PokemonList.vue
+++ src/views/PokemonList.vue
@@ -1,25 +1,11 @@
 <template>
   <div class="pokemon-list">
-    <div class="search-container">
-      <div class="search-icon-container">
-        <img src="@/assets/search.png" alt="Search" class="search-icon" />
-      </div>
-      <input 
-        v-model="searchTerm" 
-        placeholder="Search Pokémon"
-        class="search-input"
-      />
-    </div>
-
-    <div v-if="filteredPokemons.length === 0 && searchTerm" class="no-results">
-      <h1>Uh-oh!</h1>
-      <p>You look lost on your journey!</p>
-      <CustomButton 
-        text="Go back home"
-        @click="goBackHome"
-      />
-    </div>
-
+    <SearchBar v-model="searchTerm" />
+    <NoResults 
+      v-if="filteredPokemons.length === 0 && searchTerm" 
+      @go-back-home="goBackHome"
+    />
+    
     <ul v-else class="list">
       <PokemonItem 
         v-for="pokemon in filteredPokemons" 
@@ -29,7 +15,6 @@
       />
     </ul>
 
- 
     <PokemonModal 
       v-if="selectedPokemon" 
       :pokemon="selectedPokemon" 
@@ -37,34 +22,29 @@
       @close="closeModal"
     />
 
-    <div class="nav-bar">
-      <CustomButton
-        text="All"
-        icon="src/assets/list.png"
-        :backgroundColor="'#BFBFBF'"
-        @click="showAllPokemons"
-      />
-      <CustomButton
-        text="Favorites"
-        icon="src/assets/seeFavs.png"
-        @click="showFavorites"
-      />
-    </div>
+    <NavBar 
+      @show-favorites="showFavorites" 
+      @show-all="showAllPokemons"
+    />
   </div>
 </template>
 
 <script>
 import PokemonModal from "../components/PokemonModal.vue";
 import PokemonItem from "@/components/PokemonItem.vue";
-import CustomButton from "@/components/CustomButton.vue";
-import { mapState, mapActions, mapGetters } from 'vuex';
+import SearchBar from "@/components/SearchBar.vue";
+import NoResults from "@/components/NoResults.vue";
+import NavBar from "@/components/NavBar.vue";
+import { mapState, mapActions } from 'vuex';
 
 export default {
   name: "PokemonList",
   components: {
     PokemonItem,
-    CustomButton,
-    PokemonModal
+    PokemonModal,
+    SearchBar,
+    NoResults,
+    NavBar
   },
   data() {
     return {
@@ -76,25 +56,27 @@ export default {
   },
   computed: {
     ...mapState(['pokemons', 'favorites']),
-  filteredPokemons() {
-    let pokemonsToFilter = this.showFavoritesOnly ? this.pokemons.filter(pokemon => this.$store.getters.isFavorite(pokemon.name)) : this.pokemons;
-    return pokemonsToFilter.filter(pokemon =>
-      (pokemon.name || "").toLowerCase().includes((this.searchTerm || "").toLowerCase())
-    );
-  }
-},
+    filteredPokemons() {
+      let pokemonsToFilter = this.showFavoritesOnly ? this.pokemons.filter(pokemon => this.$store.getters.isFavorite(pokemon.name)) : this.pokemons;
+      return pokemonsToFilter.filter(pokemon =>
+        (pokemon.name || "").toLowerCase().includes((this.searchTerm || "").toLowerCase())
+      );
+    }
+  },
   methods: {
     ...mapActions(['fetchPokemons', 'loadMorePokemons']),
+    updateSearchTerm(newSearchTerm) {
+      this.searchTerm = newSearchTerm;
+    },
     goBackHome() {
       this.searchTerm = "";
     },
     showFavorites() {
-    this.showFavoritesOnly = true;
-  },
-  showAllPokemons() {
-    this.showFavoritesOnly = false;
-  
-},
+      this.showFavoritesOnly = true;
+    },
+    showAllPokemons() {
+      this.showFavoritesOnly = false;
+    },
     loadMore() {
       this.loadMorePokemons();
     },
@@ -113,69 +95,10 @@ export default {
 </script>
 
 <style scoped>
-.nav-bar {
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  height: 80px;
-  background-color: #ffffff;
-  box-shadow: 0px -2px 5px rgba(0, 0, 0, 0.1);
-  margin-top: auto; 
-}
-
 .pokemon-list {
   padding: 20px;
   max-width: 600px;
   margin: 0 auto;
-}
-
-.search-container {
-  position: relative;
-  width: 95%;
-  margin-bottom: 20px;
-}
-
-.search-icon-container {
-  position: absolute;
-  top: 50%;
-  left: 10px;
-  transform: translateY(-50%);
-}
-
-.search-icon {
-  width: 24px;
-  height: 24px;
-}
-
-.search-input {
-  width: 90%;
-  max-width: 570px;
-  padding: 10px 10px 10px 40px;
-  border: 1px solid #ffff;
-  box-shadow: 0px 2px 10px 0px #0000000A;
-  border-radius: 8px;
-  font-size: 22px;
-  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.1);
-}
-
-.no-results {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  text-align: center;
-  justify-content: space-around;
-}
-
-.no-results h1 {
-  font-size: 36px;
-  color: #353535;
-  font-weight: 700;
-}
-
-.no-results p {
-  font-size: 20px;
-  color: #5E5E5E;
-  font-weight: 500;
 }
 
 .list {
